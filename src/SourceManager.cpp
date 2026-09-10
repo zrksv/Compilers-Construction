8cfecc71823c18f623ec31e17b4e9e45a530c77d
zrksv <annazyranova3074@gmail.com>
2026-09-10 23:31:29 +0300

    Character reader

--- src/SourceManager.cpp
+++ src/SourceManager.cpp
@@ -1,2 +1,41 @@
 #include "SourceManager.hpp"
 
+bool SourceManager::is_eof() const {
+    return cursor >= source.size();
+}
+
+char SourceManager::peek() const {
+    if (is_eof()) {
+        return '\0';
+    }
+    return source[cursor];
+}
+
+char SourceManager::peek_next() const {
+    if (cursor + 1 >= source.size()) {
+        return '\0';
+    }
+    return source[cursor + 1];
+}
+
+Location SourceManager::get_location() const {
+    return location;
+}
+
+char SourceManager::get() {
+    if (is_eof()) {
+        return '\0';
+    }
+
+    char c = source[cursor];
+    cursor++;
+
+    if (c == '\n') {
+        location.row++;
+        location.column = 1;
+    } else {
+        location.column++;
+    }
+
+    return c;
+}
