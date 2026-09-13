8836690f84f798a27a933f435c8a3306e647abb6
I-Leonid-I <gunkoleonid06@gmail.com>
2026-09-13 03:26:01 +0300

    test samples uncommented

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -331,22 +331,22 @@ int main() {
     test_numbers();
     test_operators();
 
-    run_sample("01_print.imp");
-    // run_sample("02_variables.imp");
-    // run_sample("03_expressions.imp");
-    // run_sample("04_types_and_casting.imp");
-    // run_sample("05_logic_operators.imp");
-    // run_sample("06_scoping.imp");
-    // run_sample("07_while_loop.imp");
-    // run_sample("08_for_loop.imp");
-    // run_sample("09_routines.imp");
-    // run_sample("10_forward_declaration.imp");
-    // run_sample("10_error_no_forward.imp");
-    // run_sample("11_binsearch.imp", true);
-    // run_sample("12_product_except_self.imp", true);
-    // run_sample("13_crossover.imp", true);
-    // run_sample("14_heap.imp", true);
-    // run_sample("15_graph_dfs.imp", true);
+    run_sample("01_print.imp", true);
+    run_sample("02_variables.imp", true);
+    run_sample("03_expressions.imp", true);
+    run_sample("04_types_and_casting.imp", true);
+    run_sample("05_logic_operators.imp", true);
+    run_sample("06_scoping.imp", true);
+    run_sample("07_while_loop.imp", true);
+    run_sample("08_for_loop.imp", true);
+    run_sample("09_routines.imp", true);
+    run_sample("10_forward_declaration.imp", true);
+    run_sample("10_error_no_forward.imp", true);
+    run_sample("11_binsearch.imp", true);
+    run_sample("12_product_except_self.imp", true);
+    run_sample("13_crossover.imp", true);
+    run_sample("14_heap.imp", true);
+    run_sample("15_graph_dfs.imp", true);
 
     return 0;
 }
